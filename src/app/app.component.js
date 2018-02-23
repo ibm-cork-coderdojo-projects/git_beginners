827b1d3a648fae1aab13bd4708a5ae20b19d3539
Chris Murphy <CMurphy6@ie.ibm.com>
2018-02-23 23:02:33 +0000

    Add comments for code added Added template and templateURL for injecting html Add html app component Move html to the html component

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,3 +1,7 @@
+/*
+* 1.1 We want to access component decorator in angular lib
+* 1.2 import bootstrap method from angular lib to use in are app.
+* */
 "use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -5,17 +9,28 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var core_1 = require("@angular/core");
+var core_1 = require("@angular/core"); // 1.1
+//import { bootstrap } from '@angular/platform-browser-dynamic'; // 1.2
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'CoderDojos';
+        /*
+        * 3.1 This is where we can define any variables or functions that our components templates can use.
+        * */
+        this.name = 'New CoderDojos_2 HTML...'; // 3.1
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
+        /*
+        * 2.1. Pass in the main index html class selector
+        * 2.2. New way of injecting html code
+        * 2.3. Old way of injecting html code
+        * 2.4. Beginner way to show html code
+        * */
         selector: 'my-app',
-        template: "<h1>Hello {{name}}</h1>",
+        //template: require('./app.component.html'),                 // 2.2
+        templateUrl: './app.component.html',
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,34 @@
-import { Component } from '@angular/core';
+/*
+* 1.1 We want to access component decorator in angular lib
+* 1.2 import bootstrap method from angular lib to use in are app.
+* */
+
+import { Component } from '@angular/core';                     // 1.1
+//import { bootstrap } from '@angular/platform-browser-dynamic'; // 1.2
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+    /*
+    * 2.1. Pass in the main index html class selector
+    * 2.2. New way of injecting html code
+    * 2.3. Old way of injecting html code
+    * 2.4. Beginner way to show html code
+    * */
+    selector: 'my-app',                                     // 2.1
+    //template: require('./app.component.html'),                 // 2.2
+    templateUrl: './app.component.html',                    // 2.3
+    //template: `<h1>Hello_2 {{name}}</h1>`,                // 2.4
 })
-export class AppComponent  { name = 'CoderDojos'; }
+export class AppComponent {
+    /*
+    * 3.1 This is where we can define any variables or functions that our components templates can use.
+    * */
+
+    name = 'New CoderDojos_2 HTML...';  // 3.1
+
+}
+
+
+
+
+
+
