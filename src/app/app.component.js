40c262defb36ac6d74c333e4a4ff5d2428e8f9b9
Chris Murphy <CMurphy6@ie.ibm.com>
2018-04-14 01:00:20 +0100

    Upgrade Angularjs 4 to Angularjs 5

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -1,14 +1,15 @@
+"use strict";
 /*
 * 1.1 We want to access component decorator in angular lib
 * 1.2 import bootstrap method from angular lib to use in are app.
 * */
-"use strict";
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core"); // 1.1
 //import { bootstrap } from '@angular/platform-browser-dynamic'; // 1.2
 var AppComponent = (function () {
@@ -20,19 +21,19 @@ var AppComponent = (function () {
         this.image = 'assets/img/coderdojo_trans.png';
         this.colour = 'Blu';
     }
+    AppComponent = __decorate([
+        core_1.Component({
+            /*
+            * 2.1. Pass in the main index html class selector
+            * 2.2. New way of injecting html code
+            * 2.3. Old way of injecting html code
+            * 2.4. Beginner way to show html code
+            * */
+            selector: 'my-app',
+            //template: require('./app.component.html'),                 // 2.2
+            templateUrl: './app.component.html',
+        })
+    ], AppComponent);
     return AppComponent;
 }());
-AppComponent = __decorate([
-    core_1.Component({
-        /*
-        * 2.1. Pass in the main index html class selector
-        * 2.2. New way of injecting html code
-        * 2.3. Old way of injecting html code
-        * 2.4. Beginner way to show html code
-        * */
-        selector: 'my-app',
-        //template: require('./app.component.html'),                 // 2.2
-        templateUrl: './app.component.html',
-    })
-], AppComponent);
 exports.AppComponent = AppComponent;
